0aae973b8f7d3b5d05ff54c3ef3f22e069e503fc
nagase ryunosuke <nagase0926@gmail.com>
2025-05-29 15:42:58 +0900

    Upgrade packages

--- lib/src/calc_controller.dart
+++ lib/src/calc_controller.dart
@@ -9,8 +9,9 @@ class CalcController extends ChangeNotifier {
   String _acLabel = 'AC';
 
   /// Create a [CalcController] with [maximumDigits] is 10 and maximumFractionDigits of [numberFormat] is 6.
-  CalcController({maximumDigits = 10})
-      : _calc = Calculator(maximumDigits: maximumDigits);
+  CalcController({
+    int maximumDigits = 10,
+  }) : _calc = Calculator(maximumDigits: maximumDigits);
 
   /// Create a [Calculator].
   CalcController.numberFormat(intl.NumberFormat numberFormat, int maximumDigits)
--- lib/src/calculator.dart
+++ lib/src/calculator.dart
@@ -92,7 +92,7 @@ class CalcDisplay {
 
 /// Expression for the [Calculator].
 class CalcExpression {
-  final Parser parser = Parser();
+  final parser = GrammarParser();
   final ContextModel evaluatorContext = ContextModel();
   final String zeroDigit;
 
@@ -210,7 +210,7 @@ class Calculator {
   final int maximumDigits;
 
   /// Create a [Calculator] with [maximumDigits] is 10 and maximumFractionDigits of [numberFormat] is 6.
-  Calculator({maximumDigits = 10})
+  Calculator({int maximumDigits = 10})
       : this.numberFormat(
             NumberFormat()..maximumFractionDigits = 6, maximumDigits);
 
@@ -221,13 +221,13 @@ class Calculator {
         _display = CalcDisplay(numberFormat, maximumDigits);
 
   /// Display string
-  get displayString => _display.string;
+  String get displayString => _display.string;
 
   /// Display value
-  get displayValue => _display.value;
+  double get displayValue => _display.value;
 
   /// Expression
-  get expression => _expression.value;
+  String get expression => _expression.value;
 
   /// Set the value.
   void setValue(double val) {
